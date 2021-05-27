7d8182873851041789ad27d4aee4ff468a73fdfe
541711153@qq.com <541711153lcc>
2021-05-27 09:19:16 +0800

    feature: 动态规划第一个案例

---
+++ algorithm-common/src/main/java/com/algorithm/common/sort/CommonSort.java
@@ -0,0 +1,28 @@
+package com.algorithm.common.sort;
+
+/**
+ * @author: chuanchuan.lcc
+ * @date: 2021-05-24 18:59
+ * @modifiedBy: chuanchuan.lcc
+ * @version: 1.0
+ * @description:
+ */
+public class CommonSort {
+
+    /**
+     * 冒泡排序
+     */
+//    private void sortArray(arr) {
+//        let tmp;
+//        for (let i = 0; i < arr.length-1;i++) {
+//            for(let j = 0 ; j < arr.length-1;j++){
+//                if(arr[j] > arr[j+1]){
+//                    tmp = arr[j];
+//                    arr[j] = arr[j+1];
+//                    arr[j+1] = tmp;
+//                }
+//            }
+//        }
+//    }
+
+}
