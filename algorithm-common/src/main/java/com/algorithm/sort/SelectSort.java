1fd34ffd43b8dfc51ae719fec64f9ba81d5cfe7c
541711153@qq.com <541711153lcc>
2021-10-17 15:28:40 +0800

    feature: 选择排序性能测试

--- algorithm-common/src/main/java/com/algorithm/sort/SelectSort.java
+++ algorithm-common/src/main/java/com/algorithm/sort/SelectSort.java
@@ -7,17 +7,31 @@ import com.algorithm.utils.ArraysUtils;
  * @date: 2021-09-17 20:32
  * @modifiedBy: chuanchuan.lcc
  * @version: 1.0
- * @description:  选择排序
+ * @description: 选择排序
  */
 public class SelectSort {
 
-    public static void selectSortDemo1(int[] arr){
-        for (int i = 0; i < arr.length -1 ; i++) {
+    public static void selectSortDemo2(int[] arr) {
+        long l = System.currentTimeMillis();
+        for (int i = 0; i < arr.length - 1; i++) {
             int minPos = i;
-            for (int j = i+1; j < arr.length; j++) {
-                minPos = arr[j] < arr[minPos] ? j: minPos;
+            for (int j = i + 1; j < arr.length; j++) {
+                minPos = arr[j] < arr[minPos] ? j : minPos;
             }
-            swap(arr,i,minPos);
+            swap(arr, i, minPos);
+        }
+        long e = System.currentTimeMillis();
+        System.out.println(e - l);
+        ArraysUtils.printIntArray(arr);
+    }
+
+    public static void selectSortDemo1(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                minPos = arr[j] < arr[minPos] ? j : minPos;
+            }
+            swap(arr, i, minPos);
             ArraysUtils.printIntArray(arr);
         }
         ArraysUtils.printIntArray(arr);
--- algorithm-common/src/test/java/com/algorithm/sort/SelectSortTest.java
+++ algorithm-common/src/test/java/com/algorithm/sort/SelectSortTest.java
@@ -18,4 +18,18 @@ public class SelectSortTest {
         int[] arr = {1,5,2,4,2,7,9};
         SelectSort.selectSortDemo1(arr);
     }
+
+
+    /**
+     * 测试点： 选择排序测试性能
+     */
+    @Test
+    public void selectSortDemo2() {
+        int count = 80000;
+        int[] arr =new  int[count];
+        for (int i = 0; i < count; i++) {
+            arr[i]= (int) (Math.random()*80000);
+        }
+        SelectSort.selectSortDemo2(arr);
+    }
 }
