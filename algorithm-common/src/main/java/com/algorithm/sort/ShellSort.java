ac96970ea7a09b849c749535033cefeb2d90becd
541711153@qq.com <541711153lcc>
2021-10-24 15:00:47 +0800

    feature: 【算法】希尔排序 推导方法  https://blog.csdn.net/qq_21383435/article/details/120933829

--- algorithm-common/src/main/java/com/algorithm/sort/ShellSort.java
+++ algorithm-common/src/main/java/com/algorithm/sort/ShellSort.java
@@ -2,6 +2,8 @@ package com.algorithm.sort;
 
 import com.algorithm.utils.ArraysUtils;
 
+import java.util.Arrays;
+
 /**
  * @author: chuanchuan.lcc
  * @date: 2021-09-19 13:47
@@ -115,4 +117,97 @@ public class ShellSort {
         arr[a] = arr[a] - arr[b];
     }
 
+
+    /**
+     * 测试点：一步步推导出来希尔排序
+     *
+     * @param arr
+     */
+    public static void tuidao(int[] arr) {
+
+        for (int i = 5; i < arr.length; i++) {
+            for (int j = i - 5; 0 <= j; j -= 5) {
+                int left = arr[j];
+                int right = arr[j + 5];
+                if (left > right) {
+                    int temp = 0;
+                    temp = left;
+                    arr[j] = right;
+                    arr[j + 5] = temp;
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+        System.out.println("第一次排序" + Arrays.toString(arr));
+
+
+        for (int i = 2; i < arr.length; i++) {
+            for (int j = i - 2; 0 <= j; j -= 2) {
+                int left = arr[j];
+                int right = arr[j + 2];
+                if (left > right) {
+                    int temp = 0;
+                    temp = left;
+                    arr[j] = right;
+                    arr[j + 2] = temp;
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+        System.out.println("第2次排序" + Arrays.toString(arr));
+
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i - 1; 0 <= j; j -= 1) {
+                int left = arr[j];
+                int right = arr[j + 1];
+                if (left > right) {
+                    int temp = 0;
+                    temp = left;
+                    arr[j] = right;
+                    arr[j + 1] = temp;
+                }
+                System.out.println(Arrays.toString(arr));
+            }
+        }
+        System.out.println("第3次排序" + Arrays.toString(arr));
+    }
+
+    public static void tuidao1(int[] arr) {
+        int count = 0;
+        for (int gap = arr.length / 2; 0 < gap; gap /= 2) {
+            System.out.println("步长：" + gap + " 第" + count + "次排序");
+            for (int i = gap; i < arr.length; i++) {
+                for (int j = i - gap; 0 <= j; j -= gap) {
+                    int left = arr[j];
+                    int right = arr[j + gap];
+                    if (left > right) {
+                        int temp = 0;
+                        temp = left;
+                        arr[j] = right;
+                        arr[j + gap] = temp;
+                    }
+                    System.out.println(Arrays.toString(arr));
+                }
+
+            }
+            System.out.println("排序结果" + Arrays.toString(arr));
+        }
+    }
+
+    public static int[] weiyi(int[] arr) {
+        //增量gap，并逐步缩小增量
+        for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+            //从第gap个元素，逐个对其所在组进行直接插入排序操作
+            for (int i = gap; i < arr.length; i++) {
+                int j = i;
+                while (j - gap >= 0 && arr[j] < arr[j - gap]) {
+                    //插入排序采用交换法
+                    swap(arr, j, j - gap);
+                    j -= gap;
+                }
+            }
+        }
+        return arr;
+    }
 }
--- algorithm-common/src/test/java/com/algorithm/sort/ShellSortTest.java
+++ algorithm-common/src/test/java/com/algorithm/sort/ShellSortTest.java
@@ -2,6 +2,8 @@ package com.algorithm.sort;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 /**
@@ -15,23 +17,79 @@ public class ShellSortTest {
 
     @Test
     public void sort2() {
-        int[] arr = {9,5,1,7,10};
+        int[] arr = {9, 5, 1, 7, 10};
         ShellSort.sort2(arr);
     }
 
     @Test
     public void sort() {
-        int[] arr = {9,5,1,7,10};
+        int[] arr = {9, 5, 1, 7, 10};
         ShellSort.sort(arr);
     }
 
     /**
      * 测地点：测试希尔排序的简单大小最优值
-     *  h = 3*h+1
+     * h = 3*h+1
      */
     @Test
     public void kunthSort() {
-        int[] arr = {9,5,1,7,10};
+        int[] arr = {9, 5, 1, 7, 10};
         ShellSort.kunthSort(arr);
+    }
+
+    /**
+     * 测地点： 希尔排序推导
+     */
+    @Test
+    public void tuidao() {
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        ShellSort.tuidao(arr);
+    }
+
+    /**
+     * 测地点： 希尔排序推导
+     */
+    @Test
+    public void tuidao1() {
+        int[] arr = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        ShellSort.tuidao1(arr);
+    }
+
+    /**
+     * 测地点： 希尔排序 8万的性能测试
+     */
+    @Test
+    public void tuidaoXingNeng() {
+        int count = 80000;
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++) {
+            arr[i] = (int) (Math.random() * 80000);
+        }
+        long start = System.currentTimeMillis();
+        ShellSort.tuidao1(arr);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+    }
+
+    /**
+     * 测地点： 希尔排序 位移方法
+     *
+     * 恐怖如斯  测试性能不到1秒
+     * 105 毫秒
+     *
+     * 地址：https://blog.csdn.net/qq_21383435/article/details/120933829
+     */
+    @Test
+    public void weiyi() {
+        int count = 80000;
+        int[] arr = new int[count];
+        for (int i = 0; i < count; i++) {
+            arr[i] = (int) (Math.random() * 80000);
+        }
+        long start = System.currentTimeMillis();
+        arr = ShellSort.weiyi(arr);
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
+        System.out.println(Arrays.toString(arr));
     }
 }
