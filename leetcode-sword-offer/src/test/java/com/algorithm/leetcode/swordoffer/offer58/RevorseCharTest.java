9cd69c51e011c6a2ffdd88ef11074cde4d1d228f
541711153@qq.com <541711153lcc>
2021-11-27 11:59:11 +0800

    feature:【算法】剑指 Offer 58 - II. 左旋转字符串

---
+++ leetcode-sword-offer/src/test/java/com/algorithm/leetcode/swordoffer/offer58/RevorseCharTest.java
@@ -0,0 +1,100 @@
+package com.algorithm.leetcode.swordoffer.offer58;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author: chuanchuan.lcc
+ * @date: 2021-11-26 17:36
+ * @modifiedBy: chuanchuan.lcc
+ * @version: 1.0
+ * @description:
+ */
+public class RevorseCharTest {
+
+    RevorseChar revorseChar = new RevorseChar();
+
+    /**
+     * 测试组装的：118194 纳秒
+     */
+    @Test
+    public void reverseLeftWords() {
+        long start = System.nanoTime();
+
+        String data = "abcedcvrsdgdhgyrhrthjufr";
+        String s = revorseChar.reverseLeftWords(data, 10);
+        System.out.println(s);
+        long end = System.nanoTime();
+        System.out.println(end - start);
+
+    }
+
+    @Test
+    public void reverseLeftWords1() {
+        long start = System.nanoTime();
+
+        String data = "abcdf";
+        String s = revorseChar.reverseLeftWords1(data, 2);
+        System.out.println(s);
+        long end = System.nanoTime();
+        System.out.println(end - start);
+
+    }
+
+    /**
+     * 测试点：测试自己写的
+     * gdhgyrhrthjufredcvrsdgdh
+     * 274570
+     * 为毛耗时更多了？
+     */
+    @Test
+    public void reverseLeftWords1_1() {
+        long start = System.nanoTime();
+
+        String data = "gdhgyrhrthjufredcvrsdgdh";
+        String s = revorseChar.reverseLeftWords1(data, 10);
+        System.out.println(s);
+        long end = System.nanoTime();
+        System.out.println(end - start);
+
+    }
+
+
+    /**
+     * 测试点：别人的
+     * gdhgyrhrthjufredcvrsdgdh
+     * 105689
+     *  好像更快了
+     */
+    @Test
+    public void reverseLeftWords2() {
+        long start = System.nanoTime();
+
+        String data = "gdhgyrhrthjufredcvrsdgdh";
+        String s = revorseChar.reverseLeftWords2(data, 10);
+        System.out.println(s);
+        long end = System.nanoTime();
+        System.out.println(end - start);
+
+    }
+
+    /**
+     * 测试点：双指针
+     * gdhgyrhrthjufredcvrsdgdh
+     * 105689
+     * 135503 相比之下慢了点
+     */
+    @Test
+    public void reverseLeftWords3() {
+        long start = System.nanoTime();
+
+        String data = "abcdf";
+        String s = revorseChar.reverseLeftWords3(data, 2);
+        System.out.println(s);
+        long end = System.nanoTime();
+        System.out.println(end - start);
+
+    }
+
+}
