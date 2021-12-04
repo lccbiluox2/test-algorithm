2209c87acbc1d9b702f7fe6e0961a700cf2a50b3
541711153@qq.com <541711153lcc>
2021-12-04 19:18:03 +0800

    feature:【算法】剑指 Offer 35. 复杂链表的复制

---
+++ leetcode-sword-offer/src/main/java/com/algorithm/leetcode/swordoffer/offer35/ComplexLinkCopy.java
@@ -0,0 +1,52 @@
+package com.algorithm.leetcode.swordoffer.offer35;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author: chuanchuan.lcc
+ * @date: 2021-12-04 18:12
+ * @modifiedBy: chuanchuan.lcc
+ * @version: 1.0
+ * @description:
+ *
+ * 剑指 Offer 35. 复杂链表的复制
+ */
+public class ComplexLinkCopy {
+
+    private Map<Node,Node> map = new HashMap<>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return head;
+        }
+        //map中存的是(原节点，拷贝节点)的一个映射
+        Map<Node, Node> map = new HashMap<>();
+        for (Node cur = head; cur != null; cur = cur.next) {
+            map.put(cur, new Node(cur.val));
+        }
+        //将拷贝的新的节点组织成一个链表
+        for (Node cur = head; cur != null; cur = cur.next) {
+            map.get(cur).next = map.get(cur.next);
+            map.get(cur).random = map.get(cur.random);
+        }
+
+        return map.get(head);
+    }
+
+}
+
+class Node {
+
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+}
