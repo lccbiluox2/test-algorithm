b28fa35e934ee48172f87fc2237fe3da485fcb6a
541711153@qq.com <541711153lcc>
2021-12-05 10:23:45 +0800

    feature:【算法】剑指 Offer 59 - I. 滑动窗口的最大值

---
+++ leetcode-sword-offer/src/main/java/com/algorithm/leetcode/swordoffer/offer59/MaxQueue.java
@@ -0,0 +1,49 @@
+package com.algorithm.leetcode.swordoffer.offer59;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author: chuanchuan.lcc
+ * @date: 2021-12-04 20:26
+ * @modifiedBy: chuanchuan.lcc
+ * @version: 1.0
+ * @description: 剑指 Offer 59 - II. 队列的最大值
+ */
+public class MaxQueue {
+    private Deque<Integer> queue;
+    private Deque<Integer> help;
+
+    public MaxQueue() {
+        queue = new ArrayDeque<>();
+        help = new ArrayDeque<>();
+    }
+
+    public int max_value() {
+        return queue.isEmpty() ? -1 : help.peek();
+    }
+
+    public void push_back(int value) {
+        queue.offer(value);
+        while(!help.isEmpty() && value > help.peekLast()) {
+            help.pollLast();
+        }
+        help.offer(value);
+    }
+
+    public int pop_front() {
+        if(queue.isEmpty()) {
+            return -1;
+        }
+        int val = queue.pop();
+        if(help.peek() == val) {
+            help.pop();
+        }
+        return val;
+    }
+}
