47b3fd08f3bfb2853efa6f2ccb21e18078fbc091
541711153@qq.com <541711153lcc>
2021-12-19 16:18:05 +0800

    feature: 【算法】剑指 Offer 46. 把数字翻译成字符串

---
+++ leetcode-sword-offer/src/test/java/com/algorithm/leetcode/swordoffer/offer46/NumTranstoStringTest.java
@@ -0,0 +1,30 @@
+package com.algorithm.leetcode.swordoffer.offer46;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author: chuanchuan.lcc
+ * @date: 2021-12-15 22:07
+ * @modifiedBy: chuanchuan.lcc
+ * @version: 1.0
+ * @description:
+ */
+public class NumTranstoStringTest {
+
+    private NumTranstoString numTranstoString = new NumTranstoString();
+
+    @Test
+    public void translateNum() {
+        int i = numTranstoString.translateNum(12258);
+        System.out.println(i);
+    }
+
+
+    @Test
+    public void translateNum1_1() {
+        int i = numTranstoString.translateNum1(12258);
+        System.out.println(i);
+    }
+}
